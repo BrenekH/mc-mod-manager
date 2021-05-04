b9c3fc235ff2b0957e5eb1e35151aeb7ecdbd712
Brenek Harrison <brenekharrison@gmail.com>
2021-05-04 15:02:11 -0600

    Get latest GH Release

--- mc_mod/github/__init__.py
+++ mc_mod/github/__init__.py
@@ -15,23 +15,52 @@ save_dir = Path(f"{os.getenv('LOCALAPPDATA')}/mc_mod/cache/github")
 save_dir.mkdir(parents=True, exist_ok=True)
 
 def download_mod(info: Dict) -> Path:
-	repo_url = _sanitize_url(info["repo_url"])
-
-	with (Path.cwd() / "test.jar").open("w") as f:
-		f.write("Hi")
+	repo = info["repo"]
 
 	if info["releases"] != None:
-		return _releases(repo_url, info["releases"])
+		return _releases(repo, info["releases"])
 	elif info["compile"] != None:
-		return _compile(repo_url, info["compile"])
+		return _compile(repo, info["compile"])
 	else:
 		raise RuntimeError("'releases' or 'compile' must be defined")
 
-def _releases(repo_url: str, info: Dict) -> Path:
+def _releases(repo: str, info: Dict) -> Path:
 	if info["latest"]:
-		return Path.cwd() / "test.jar"
+		r = requests.get(f"https://api.github.com/repos/{repo}/releases/latest")
+		r.raise_for_status()
+
+		latest_release = r.json()
+
+		for asset in latest_release["assets"]:
+			invalid = False
+			for s in info["must_contain"]:
+				if s not in asset["name"]:
+					invalid = True
+					break
+			if invalid:
+				continue
+
+			for s in info["must_not_contain"]:
+				if s in asset["name"]:
+					invalid = True
+					break
+			if invalid:
+				continue
+
+			r = requests.get(asset["browser_download_url"])
+			r.raise_for_status()
+
+			out_file: Path = save_dir / asset["name"]
+
+			with out_file.open("wb") as f:
+				f.write(r.content)
+
+			return out_file
+
+		raise RuntimeError(f"Could not locate a valid binary for {info}")
+
 	else: # We have to evaluate all tags against info["tag"]
-		matching_releases = [release for release in get_releases(repo_url.replace("https://github.com/", "").replace("http://github.com/", "")) if info["tag"] in release["tag_name"]]
+		matching_releases = [release for release in get_releases(repo) if info["tag"] in release["tag_name"]]
 
 		times = [isoparse(r["published_at"]) for r in matching_releases]
 
@@ -67,7 +96,7 @@ def _releases(repo_url: str, info: Dict) -> Path:
 
 		raise RuntimeError(f"Could not locate a valid binary for {info}")
 
-def _compile(repo_url: str, info: Dict) -> Path:
+def _compile(repo: str, info: Dict) -> Path:
 	return Path.cwd() / "test.jar"
 
 def _sanitize_url(url: str) -> str:
