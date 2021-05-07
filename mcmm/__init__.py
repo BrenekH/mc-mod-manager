5d08a72aaa35d9c4f064cd3e6fc40289561f2202
Brenek Harrison <brenekharrison@gmail.com>
2021-05-06 23:03:45 -0600

    Load external mod providers from user config file

--- mcmm/__init__.py
+++ mcmm/__init__.py
@@ -1,8 +1,10 @@
+from json import load
 from sys import argv
 
 from . import curse_forge, github, optifine
 from .commands import _activate_dispatcher
 from .commands import _download_dispatcher
+from .dirs import gen_config_dir
 from .plugin_internal import load_providers
 
 __version__ = "0.0.1-alpha.1"
@@ -16,5 +18,16 @@ def cli():
 		_activate_dispatcher(argv[2:])
 
 	elif command == "download":
-		mod_providers = load_providers([curse_forge, optifine, github])
+		internal_mps = [curse_forge, optifine, github]
+		config_dir = gen_config_dir()
+		config_file = config_dir / "config.json"
+
+		if not config_file.exists():
+			with config_file.open("w") as f:
+				f.write(r'{"mod_providers": []}')
+
+		with config_file.open("r") as f:
+			user_conf = load(f)
+
+		mod_providers = load_providers(user_conf["mod_providers"] + internal_mps)
 		_download_dispatcher(argv[2:], mod_providers)
